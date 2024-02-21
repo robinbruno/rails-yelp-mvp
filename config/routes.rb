1086fd0bfe5f3cfc6982f0ca70f9f2f0c66ce584
Romain Bruzeau <bruzeau.romain@gmail.com>
2024-02-21 16:45:05 +0100

    YELP-new-form-no-button

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/id', to: 'restaurants#show', as: :restaurant
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
 end
